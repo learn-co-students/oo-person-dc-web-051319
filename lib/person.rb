da239c4be1a675e5a081b199dd322149c2a77e9e
villatrue <scarter0313@gmail.com>
2019-05-15 17:26:54 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,25 @@
-# your code goes here
+# your code goes hereclass
+class Person
+    attr_accessor :bank_account, :happiness, :hygeine
+    attr_reader :name
+
+    def initialize(name)
+      @name = name
+      @bank_account = 25
+      @happiness = 8
+      @hygeine = 8
+    end
+
+    # def clean?
+    #   if @hygeine > 7
+    #     puts true
+    #   else puts false
+    #   end
+    # end
+    # def happy?
+    #   if @happiness> 7
+    #     puts true
+    #   else puts false
+    #   end
+    # end
+end
