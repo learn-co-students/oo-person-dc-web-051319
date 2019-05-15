38a9faba8f318f04679da095ff7bf75e2b8de344
Matt Williams <matt.seongjun@gmail.com>
2019-05-15 15:28:14 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,81 @@
-# your code goes here
+class Person
+
+    MIN = 0
+    MAX = 10
+
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+
+    def initialize(name, bank_account=25, happiness=8, hygiene=8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = correct_range(happiness)
+        @hygiene = correct_range(hygiene)
+    end
+
+    def happiness=(happiness)
+        @happiness = correct_range(happiness)
+    end
+
+    def hygiene=(hygiene)
+        @hygiene = correct_range(hygiene)
+    end
+    
+    def correct_range(n)
+        case 
+            when n < MIN
+                0
+            when n > MAX
+                10
+            else
+                n 
+            end
+    end
+    
+    def happy?
+        self.happiness > 7
+    end
+
+    def clean?
+        self.hygiene > 7
+    end
+
+    def get_paid(salary)
+        self.bank_account += salary
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        return  "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+
+        case topic
+        when "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            return "blah blah partisan blah lobbyist"
+        when "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            return "blah blah sun blah rain"
+        else
+            return "blah blah blah blah blah"
+        end
+    end
+
+end
